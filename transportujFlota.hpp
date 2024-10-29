0cf4e0cd8c6f0c6f25f390154f5752d690e77f15
HubLip <01171666@pw.edu.pl>
2024-10-29 17:06:25 +0100

    Zejsciowka 3.0 29.10.2024

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,28 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+    {
+        return 0; //Jezeli towar jest 0 to nie tworzymy zadnego statku
+    }
+
+    Stocznia stocznia{};
+    unsigned int iloscTowaru = 0;
+    unsigned int sumarycznyTowar = 0; 
+    unsigned int licznikZaglowcow = 0;
+
+    while (sumarycznyTowar <= towar )
+    {
+        Statek* s1 = stocznia();
+
+        iloscTowaru = s1 -> transportuj();
+        sumarycznyTowar += iloscTowaru;
+
+        if (dynamic_cast<Zaglowiec*>(s1))
+        {
+            ++licznikZaglowcow;
+        }
+        delete s1;
+    }
+    return licznikZaglowcow;
 }
